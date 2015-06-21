b528ce60ebaf8bfe7f2e3c7d60e2c2823f950cb8
jimzhan <jim.zhan@me.com>
2015-06-21 18:43:13 +0800

    init imports with travis

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# cmd
-cmd is a command line toolkit for the Golang.
+cmd
+===
+[![Build Status](https://travis-ci.org/goanywhere/cmd.svg?branch=master)](https://travis-ci.org/goanywhere/cmd) [![GoDoc](https://godoc.org/github.com/goanywhere/cmd?status.svg)](http://godoc.org/github.com/goanywhere/cmd)
+
+cmd is a command line toolkit for the [Go](http://golang.org) programming language.
+
+## Getting Started
+
+Install the package, along with executable binary helper (**go 1.4** and greater is required):
+
+```shell
+$ go get -v github.com/goanywhere/cmd
+```
---
+++ message.go
@@ -0,0 +1,61 @@
+/* ----------------------------------------------------------------------
+ *       ______      ___                         __
+ *      / ____/___  /   |  ____  __  ___      __/ /_  ___  ________
+ *     / / __/ __ \/ /| | / __ \/ / / / | /| / / __ \/ _ \/ ___/ _ \
+ *    / /_/ / /_/ / ___ |/ / / / /_/ /| |/ |/ / / / /  __/ /  /  __/
+ *    \____/\____/_/  |_/_/ /_/\__. / |__/|__/_/ /_/\___/_/   \___/
+ *                            /____/
+ *
+ * (C) Copyright 2015 GoAnywhere (http://goanywhere.io).
+ * ----------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ----------------------------------------------------------------------*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+)
+
+// Loading provides a simple dotted loading prompt while
+// processing task. NOTE that task needs to manually notify
+// the prompt once the task is done.
+func Loading(done chan bool) {
+	var (
+		index      = 0
+		indicators = []string{"-", "\\", "|", "/"}
+	)
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Print(" \b")
+				close(done)
+				return
+
+			default:
+				fmt.Printf("%s\b", indicators[index])
+				if index < (len(indicators) - 1) {
+					index++
+				} else {
+					index = 0
+				}
+				time.Sleep(50 * time.Millisecond)
+			}
+		}
+	}()
+}
+
+func Prompt(message string, values ...interface{}) {
+	fmt.Printf(fmt.Sprintf("* %s", message), values...)
+}
